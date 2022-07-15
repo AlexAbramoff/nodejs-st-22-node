05bc7f88d44b57bd13341ae9270ad64c3897c664
Alexey Abramov <alex93178@gmail.com>
2022-07-15 15:09:15 +0300

    complete task 1

---
+++ index.js
@@ -0,0 +1,6 @@
+const process = require('process');
+
+process.stdin.on('data', (data) => {
+    const res = data.toString('utf8').split('').reverse().join('')
+    process.stdout.write(res+'\n')
+    })
